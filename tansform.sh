fb065db7992c24a38696b2d319e4753e7ffa765a
Jay <jpnappy@gmail.com>
2016-01-06 14:12:45 -0500

    transform.sh

---
+++ tansform.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# Script to transform teacher directories to student directories
+
+echo "Deleteing solution code"
+find . -name 'solution-code' -exec rm -rf {} \;
+
+echo "Deleting lab folders"
+find . -name '*-lab' -exec rm -rf {} \;
+
+echo "Deleting instructor comments and checks"
+find . -type f -name 'readme.md' -print0 | xargs -0 sed -i '' '/^>/ d'
